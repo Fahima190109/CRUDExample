cfaf23d961f96370e36add52096066b15fd621a1
Fahima190109 <fahimaakhter4326@outlook.com>
2025-11-29 11:10:07 +0600

    Add project files.

---
+++ CRUDTests/CountriesServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace CRUDTests
+{
+    public class CountriesServiceTest
+    {
+    private readonly ICountriesService 
+            countriesService;
+    }
+}
---
+++ Services/CountriesService.cs
@@ -0,0 +1,12 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+namespace Services
+{
+    public class CountriesService : ICountriesService
+    {
+        public CountryResponse AddCountry(CountryResponse? countryResponse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
